aa52664d7afa8520395cb2feb7f6d90bc78467f6
edman-cota <edmancota2017@gmail.com>
2025-03-09 15:54:09 -0600

    aplicamos mejoras de mensaje directo y encriptaciones

--- client.c
+++ client.c
@@ -4,56 +4,41 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <pthread.h>
+#include <ncurses.h>
+#include <openssl/aes.h>
 
 #define BUFFER_SIZE 1024
+const unsigned char AES_KEY[16] = "1234567890123456"; // Clave AES (Ejemplo)
 
-int client_socket;
-char username[50];
+int socket_fd;
+struct sockaddr_in server_addr;
 
-void *receive_messages(void *arg);
-
-int main(int argc, char *argv[])
+void encrypt_message(const char *input, unsigned char *output)
 {
-    if (argc != 4)
-    {
-        printf("Uso: %s <nombre_usuario> <IP_servidor> <puerto>\n", argv[0]);
-        return -1;
-    }
+    AES_KEY encryptKey;
+    AES_set_encrypt_key(AES_KEY, 128, &encryptKey);
+    AES_encrypt((unsigned char *)input, output, &encryptKey);
+}
 
-    strcpy(username, argv[1]);
+void decrypt_message(const unsigned char *input, char *output)
+{
+    AES_KEY decryptKey;
+    AES_set_decrypt_key(AES_KEY, 128, &decryptKey);
+    AES_decrypt(input, (unsigned char *)output, &decryptKey);
+}
 
-    struct sockaddr_in server_addr;
-    client_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (client_socket == -1)
-    {
-        perror("Error al crear el socket");
-        return -1;
-    }
+void init_ncurses()
+{
+    initscr();
+    cbreak();
+    noecho();
+    scrollok(stdscr, TRUE);
+}
 
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(atoi(argv[3]));
-    inet_pton(AF_INET, argv[2], &server_addr.sin_addr);
-
-    if (connect(client_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
-    {
-        perror("Error en connect");
-        return -1;
-    }
-
-    send(client_socket, username, strlen(username), 0);
-
-    pthread_t tid;
-    pthread_create(&tid, NULL, receive_messages, NULL);
-
-    char buffer[BUFFER_SIZE];
-    while (1)
-    {
-        fgets(buffer, BUFFER_SIZE, stdin);
-        send(client_socket, buffer, strlen(buffer), 0);
-    }
-
-    close(client_socket);
-    return 0;
+void display_message(const char *msg)
+{
+    printw("%s\n", msg);
+    refresh();
 }
 
 void *receive_messages(void *arg)
@@ -62,12 +47,69 @@ void *receive_messages(void *arg)
     while (1)
     {
         memset(buffer, 0, BUFFER_SIZE);
-        int bytes_received = recv(client_socket, buffer, sizeof(buffer), 0);
+        int bytes_received = recv(socket_fd, buffer, BUFFER_SIZE, 0);
         if (bytes_received <= 0)
         {
-            printf("Desconectado del servidor\n");
-            exit(0);
+            display_message("Desconectado del servidor. Intentando reconectar...");
+            close(socket_fd);
+            reconnect();
         }
-        printf("%s\n", buffer);
+        else
+        {
+            display_message(buffer);
+        }
     }
+    return NULL;
+}
+
+void reconnect()
+{
+    while (1)
+    {
+        socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+        if (connect(socket_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == 0)
+        {
+            display_message("Reconectado al servidor.");
+            return;
+        }
+        sleep(3);
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc < 3)
+    {
+        printf("Uso: %s <IP del servidor> <puerto>\n", argv[0]);
+        return 1;
+    }
+
+    char *server_ip = argv[1];
+    int port = atoi(argv[2]);
+
+    socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port);
+    inet_pton(AF_INET, server_ip, &server_addr.sin_addr);
+
+    if (connect(socket_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+    {
+        printf("Error al conectar con el servidor.\n");
+        return 1;
+    }
+
+    init_ncurses();
+    pthread_t receive_thread;
+    pthread_create(&receive_thread, NULL, receive_messages, NULL);
+
+    char input[BUFFER_SIZE];
+    while (1)
+    {
+        getstr(input);
+        send(socket_fd, input, strlen(input), 0);
+    }
+
+    endwin();
+    close(socket_fd);
+    return 0;
 }
--- server.c
+++ server.c
@@ -2,128 +2,46 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <arpa/inet.h>
 #include <pthread.h>
+#include <arpa/inet.h>
+#include <openssl/aes.h>
 
-#define MAX_CLIENTS 10
+#define MAX_CLIENTS 100
 #define BUFFER_SIZE 1024
+#define PORT 8080
 
 typedef struct
 {
 	int socket;
 	char username[50];
+	char status[10];
 	struct sockaddr_in address;
-	int status; // 0 = ACTIVO, 1 = OCUPADO, 2 = INACTIVO
 } Client;
 
 Client clients[MAX_CLIENTS];
 pthread_mutex_t clients_mutex = PTHREAD_MUTEX_INITIALIZER;
+const unsigned char AES_KEY[16] = "1234567890123456"; // Clave AES (Ejemplo)
 
-void broadcast_message(char *message, int sender_socket);
-void remove_client(int client_socket);
-void *client_handler(void *arg);
-
-int main(int argc, char *argv[])
+void encrypt_message(const char *input, unsigned char *output)
 {
-	if (argc != 2)
-	{
-		printf("Uso: %s <puerto>\n", argv[0]);
-		return -1;
-	}
-
-	int server_socket, client_socket;
-	struct sockaddr_in server_addr, client_addr;
-	socklen_t addr_size = sizeof(client_addr);
-	pthread_t tid;
-
-	server_socket = socket(AF_INET, SOCK_STREAM, 0);
-	if (server_socket == -1)
-	{
-		perror("Error al crear el socket del servidor");
-		return -1;
-	}
-
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_addr.s_addr = INADDR_ANY;
-	server_addr.sin_port = htons(atoi(argv[1]));
-
-	if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
-	{
-		perror("Error en bind");
-		return -1;
-	}
-
-	if (listen(server_socket, MAX_CLIENTS) < 0)
-	{
-		perror("Error en listen");
-		return -1;
-	}
-
-	printf("Servidor iniciado en el puerto %s...\n", argv[1]);
-
-	while (1)
-	{
-		client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &addr_size);
-		if (client_socket < 0)
-		{
-			perror("Error en accept");
-			continue;
-		}
-
-		pthread_t tid;
-		int *new_sock = malloc(sizeof(int));
-		*new_sock = client_socket;
-		pthread_create(&tid, NULL, client_handler, (void *)new_sock);
-	}
-
-	close(server_socket);
-	return 0;
+	AES_KEY encryptKey;
+	AES_set_encrypt_key(AES_KEY, 128, &encryptKey);
+	AES_encrypt((unsigned char *)input, output, &encryptKey);
 }
 
-void *client_handler(void *arg)
+void decrypt_message(const unsigned char *input, char *output)
 {
-	int client_socket = *(int *)arg;
-	free(arg);
-	char buffer[BUFFER_SIZE];
-
-	recv(client_socket, buffer, sizeof(buffer), 0);
-
-	pthread_mutex_lock(&clients_mutex);
-	for (int i = 0; i < MAX_CLIENTS; i++)
-	{
-		if (clients[i].socket == 0)
-		{
-			clients[i].socket = client_socket;
-			strcpy(clients[i].username, buffer);
-			clients[i].status = 0; // ACTIVO
-			break;
-		}
-	}
-	pthread_mutex_unlock(&clients_mutex);
-
-	printf("%s se ha conectado\n", buffer);
-
-	while (1)
-	{
-		memset(buffer, 0, BUFFER_SIZE);
-		int bytes_received = recv(client_socket, buffer, sizeof(buffer), 0);
-		if (bytes_received <= 0)
-		{
-			printf("Cliente desconectado\n");
-			remove_client(client_socket);
-			close(client_socket);
-			return NULL;
-		}
-		broadcast_message(buffer, client_socket);
-	}
+	AES_KEY decryptKey;
+	AES_set_decrypt_key(AES_KEY, 128, &decryptKey);
+	AES_decrypt(input, (unsigned char *)output, &decryptKey);
 }
 
-void broadcast_message(char *message, int sender_socket)
+void send_message_to_all(char *message)
 {
 	pthread_mutex_lock(&clients_mutex);
 	for (int i = 0; i < MAX_CLIENTS; i++)
 	{
-		if (clients[i].socket != 0 && clients[i].socket != sender_socket)
+		if (clients[i].socket != 0)
 		{
 			send(clients[i].socket, message, strlen(message), 0);
 		}
@@ -131,16 +49,89 @@ void broadcast_message(char *message, int sender_socket)
 	pthread_mutex_unlock(&clients_mutex);
 }
 
-void remove_client(int client_socket)
+void send_user_list(int client_socket)
 {
+	char user_list[BUFFER_SIZE] = "Usuarios conectados:\n";
 	pthread_mutex_lock(&clients_mutex);
 	for (int i = 0; i < MAX_CLIENTS; i++)
 	{
-		if (clients[i].socket == client_socket)
+		if (clients[i].socket != 0)
 		{
-			clients[i].socket = 0;
-			break;
+			strcat(user_list, clients[i].username);
+			strcat(user_list, "\n");
 		}
 	}
 	pthread_mutex_unlock(&clients_mutex);
+	send(client_socket, user_list, strlen(user_list), 0);
+}
+
+void process_command(char *buffer, int client_socket)
+{
+	if (strncmp(buffer, "/status ", 8) == 0)
+	{
+		char status[10];
+		sscanf(buffer, "/status %s", status);
+		pthread_mutex_lock(&clients_mutex);
+		for (int i = 0; i < MAX_CLIENTS; i++)
+		{
+			if (clients[i].socket == client_socket)
+			{
+				strcpy(clients[i].status, status);
+				break;
+			}
+		}
+		pthread_mutex_unlock(&clients_mutex);
+	}
+	else if (strncmp(buffer, "/users", 6) == 0)
+	{
+		send_user_list(client_socket);
+	}
+	else
+	{
+		send(client_socket, "Comando no reconocido\n", 23, 0);
+	}
+}
+
+void *handle_client(void *arg)
+{
+	int client_socket = *((int *)arg);
+	char buffer[BUFFER_SIZE];
+	while (1)
+	{
+		memset(buffer, 0, BUFFER_SIZE);
+		int bytes_received = recv(client_socket, buffer, BUFFER_SIZE, 0);
+		if (bytes_received <= 0)
+		{
+			break;
+		}
+		process_command(buffer, client_socket);
+	}
+	close(client_socket);
+	return NULL;
+}
+
+int main()
+{
+	int server_socket, client_socket;
+	struct sockaddr_in server_addr, client_addr;
+	socklen_t client_len = sizeof(client_addr);
+
+	server_socket = socket(AF_INET, SOCK_STREAM, 0);
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = INADDR_ANY;
+	server_addr.sin_port = htons(PORT);
+
+	bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr));
+	listen(server_socket, 10);
+
+	printf("Servidor escuchando en el puerto %d\n", PORT);
+
+	while (1)
+	{
+		client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &client_len);
+		pthread_t thread;
+		pthread_create(&thread, NULL, handle_client, &client_socket);
+	}
+
+	return 0;
 }
